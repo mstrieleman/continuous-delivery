54d01a1c5a1a3b5acccf9315e8b2f5ba3d611593
mstrieleman <mstrieleman@gmail.com>
2018-04-20 15:08:53 -0700

    Revised index.test.js.

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,25 @@
-const { describe, before, after, it } = require('mocha');
 const { expect } = require('chai');
+const { describe, it, before, after } = require('mocha');
 const request = require('request');
-const createApp = require('../create-app');
+const createApp = require('../create-app.js');
 
-describe('app', () => {
+describe('tests .get()', () => {
+  const app = createApp();
+  before(done => {
+    server = app.listen(3000, () => {
+      done();
+    });
+  });
+
+  after(done => {
+    server.close(() => {
+      done();
+    });
+  });
+
   describe('GET /', () => {
-    it('responds with a string of exercise description', () => {
-      request('http://localhost:3000/', (err, response, body) => {
+    it('responds with a string of exercise description', done => {
+      request('http://localhost:3000', (err, response, body) => {
         expect(err).to.equal(null);
         expect(response.statusCode).to.equal(200);
         expect(response).to.be.an('object');
